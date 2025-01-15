0204ea7f7bf5172136f111a860e08e895730216f
Alex Amedia <aamedia@asu.edu>
2025-01-14 17:36:23 -0700

    13. Exploring Spring Boot Project Files - Part 2

---
+++ mycoolapp/src/main/java/com/luv2code/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,21 @@
+package com.luv2code.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+
+@RestController
+public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // Expose "/" that returns "Hello World"
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World " + coachName + " of team " + teamName;
+    }
+}
