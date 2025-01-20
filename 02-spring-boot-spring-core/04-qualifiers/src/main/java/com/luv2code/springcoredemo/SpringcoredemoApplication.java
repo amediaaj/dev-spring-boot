238df202f860233db28acdd40aa016fc0d9b6ae0
Alex Amedia <aamedia@asu.edu>
2025-01-20 15:30:44 -0700

    04-qualifiers

---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/luv2code/springcoredemo/SpringcoredemoApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoredemoApplication {
+
+	public static void main(String[] args) {
+		// Bootstrap your Spring Boot application
+		SpringApplication.run(SpringcoredemoApplication.class, args);
+	}
+}
