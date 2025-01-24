60e64e35ec64b58fbe8e8b0fe730e6595a0b0af2
Alex Amedia <aamedia@asu.edu>
2025-01-24 14:35:04 -0700

    07-bean-scopes

---
+++ 02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,17 @@
+package com.luv2code.springcoredemo.common;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component // Marks the class as a Spring Bean and makes it available for dependency injection
+public class CricketCoach implements Coach {
+    public CricketCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes";
+    }
+}
